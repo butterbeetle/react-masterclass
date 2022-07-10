d65c8b2747b32c85966c6a18fc55ac74a3726a57
butterbeetle <butterbeetle@naver.com>
2022-07-10 17:06:37 +0900

    4.4 Route States

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,16 +1,53 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 interface RouteParams {
   coinId: string;
 }
 
-function Coin() {
-  const { coinId } = useParams<keyof RouteParams>() as RouteParams;
+interface RouteState {
+  state: {
+    name: string;
+  }
+}
 
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<keyof RouteParams>() as RouteParams;
+  const { state } = useLocation() as RouteState;
   return (
-    <div>
-      <h1>Coin: {coinId}</h1>
-    </div>
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading"}</Title>
+      </Header>
+      {
+        loading ?
+          <Loader>Loading...</Loader> : null}
+    </Container>
   );
 }
 
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -2,6 +2,16 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
 const Container = styled.div`
   padding: 0px 20px;
   max-width: 480px;
@@ -26,9 +36,10 @@ const Coin = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a{
+    display: flex;
+    align-items: center;
     padding:20px;
     transition: color 0.2s ease-in;
-    display:block;
   }
   &:hover{
     a{
@@ -37,14 +48,10 @@ const Coin = styled.li`
   }
 `;
 
-const Title = styled.h1`
-  font-size: 48px;
-  color:${props => props.theme.accentColor};
-`;
-
-const Loader = styled.span`
-  text-align: center;
-  display:block;
+const Img = styled.img`
+  width:35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -80,7 +87,8 @@ function Coins() {
           <CoinsList>
             {coins.map(coin =>
               <Coin key={coin.id}>
-                <Link to={`/${coin.id}`}>
+                <Link to={`/${coin.id}`} state={{ name: coin.name }}>
+                  <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
                   {coin.name} &rarr;
                 </Link>
               </Coin>
