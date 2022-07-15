144f26efe9ac0568b8e3d867cb3caebf03951cbc
butterbeetle <butterbeetle@naver.com>
2022-07-16 01:36:11 +0900

    4.13 Price Chart part Two

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 #3.7 npm i --save-dev @types/react-query
 #4.0 npm i react-router-dom
 #4.9 npm i react-query
+#4.13 npm install --save react-apexcharts apexcharts
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,19 @@
 import { useQuery } from "react-query";
 import { useOutletContext } from "react-router-dom";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+
+interface IHistoryData {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 interface ChartProps {
   coinId: string;
@@ -8,9 +21,47 @@ interface ChartProps {
 
 function Chart() {
   const { coinId } = useOutletContext<ChartProps>();
-  //console.log("data", coinId);
-  const { } = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
-  return <h1>Chart</h1>;
+  const { isLoading, data } = useQuery<IHistoryData[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+  return <div>{isLoading ? "Loading chart..." :
+    <ApexChart
+      type="line"
+      series={[
+        {
+          name: "price",
+          data: data?.map((price => price.close)) ?? [],
+        },
+      ]}
+      options={{
+        theme: {
+          mode: "dark",
+        },
+        chart: {
+          height: 300,
+          width: 500,
+          toolbar: {
+            show: false,
+          },
+          background: "transparent"
+        },
+        stroke: {
+          curve: "smooth",
+          width: 4,
+        },
+        grid: {
+          show: false,
+        },
+        yaxis: {
+          show: false,
+        },
+        xaxis: {
+          labels: { show: false },
+          axisTicks: { show: false },
+          axisBorder: { show: false },
+        },
+      }}
+    />
+  }
+  </div>
 }
 
 export default Chart;
