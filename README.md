b2cb9d261571849502cb51cfa6fe2fcbccbb0e24
butterbeetle <butterbeetle@naver.com>
2022-07-16 00:19:42 +0900

    4.9 React Query part One

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 #3.1 npm i styled-components
 #3.7 npm i --save-dev @types/react-query
 #4.0 npm i react-router-dom
+#4.9 npm i react-query
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 const Title = styled.h1`
   font-size: 48px;
@@ -54,7 +56,7 @@ const Img = styled.img`
   margin-right: 10px;
 `;
 
-interface CoinInterface {
+interface ICoin {
   id: string,
   name: string,
   symbol: string,
@@ -65,16 +67,7 @@ interface CoinInterface {
 }
 
 function Coins() {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins");
-      const json = await response.json();
-      setCoins(json.slice(0, 100));
-      setLoading(false);
-    })();
-  }, []);
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins)
 
   return (
     <Container>
@@ -82,10 +75,10 @@ function Coins() {
         <Title>코인</Title>
       </Header>
       {
-        loading ?
+        isLoading ?
           <Loader>Loading...</Loader> :
           <CoinsList>
-            {coins.map(coin =>
+            {data?.slice(0, 100).map(coin =>
               <Coin key={coin.id}>
                 <Link to={`/${coin.id}`} state={{ name: coin.name }}>
                   <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
