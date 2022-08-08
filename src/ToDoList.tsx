6a11e05aaf7a4ad7088012a4952239bfaf582764
butterbeetle <butterbeetle@naver.com>
2022-08-08 20:10:18 +0900

    5.10 Recap

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,92 +1,24 @@
 import { useForm } from "react-hook-form";
 
 type IFormData = {
-  errors: {
-    email: {
-      message: string;
-    };
-  };
-  firstName: string;
-  lastName: string;
-  username: string;
-  email: string;
-  password: string;
-  password1: string;
-  extraError?: string;
+  toDo: string
 };
 
 function ToDoList() {
-  const { register, handleSubmit, formState: { errors }, setError } = useForm<IFormData>({
-    defaultValues: {
-      email: "@naver.com",
-    }
-  });
-  const onValid = (data: IFormData) => {
-    if (data.password !== data.password1) {
-      setError(
-        "password1",
-        { message: "Password are not the same" },
-        { shouldFocus: true });
-    }
-    //setError("extraError", { message: "Server offline." });
+  const {
+    register, handleSubmit, setValue
+  } = useForm<IFormData>();
+  const handleValid = (data: IFormData) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", "");
   };
-  console.log(errors);
   return (
     <div>
-      <form style={{ display: "flex", flexDirection: "column" }} onSubmit={handleSubmit(onValid)}>
-        <input {...register("email", {
-          required: "Email is reuqired",
-          pattern: {
-            value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
-            message: "Only naver.com emails allowed",
-          },
-        })} placeholder="Email" />
-        <span>{errors?.email?.message}</span>
-
-        <input {...register("firstName", {
-          required: "write here",
-          validate: {
-            noNico: (value) => value.includes("nico") ? "no nicos allowed" : true,
-            noNick: (value) => value.includes("nick") ? "no nicks allowed" : true,
-          }
-        })
-        } placeholder="First Name" />
-        <span>{errors?.firstName?.message}</span>
-
-        <input {...register("lastName", { required: "write here" })} placeholder="Last Name" />
-        <span>{errors?.lastName?.message}</span>
-
-        <input {...register("username", {
-          required: "write here",
-          minLength: {
-            value: 10,
-            message: "Too short"
-          }
-        })
-        } placeholder="Username" />
-        <span>{errors?.username?.message}</span>
-
-        <input {...register("password", {
-          required: "write here",
-          minLength: {
-            value: 5,
-            message: "Too short"
-          }
-        })
-        } placeholder="Password" />
-        <span>{errors?.password?.message}</span>
-
-        <input {...register("password1", {
-          required: "Password is required",
-          minLength: {
-            value: 5,
-            message: "Your password is too short."
-          }
-        })
-        } placeholder="Password1" />
-        <span>{errors?.password1?.message}</span>
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input {...register("toDo", {
+          required: "Please write a To Do",
+        })} placeholder="Write a to do" />
         <button>Add</button>
-        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
