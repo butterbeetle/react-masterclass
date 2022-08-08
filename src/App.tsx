a6d4684a51ab8546a6e1d8666fc7327ea142258d
butterbeetle <butterbeetle@naver.com>
2022-08-08 17:24:28 +0900

    5.5 To Do Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import Router from "./Router";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { darkTheme, lightTheme } from './theme';
-import { useRecoilValue } from "recoil";
-import { isDarkAtom } from "./atoms";
+import { createGlobalStyle } from "styled-components";
+import ToDoList from "./ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -71,14 +67,12 @@ a {
 `;
 
 function App() {
-  const isDark = useRecoilValue(isDarkAtom);
-  return <>
-    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+  return (
+    <>
       <GlobalStyle />
-      <Router />
-      <ReactQueryDevtools initialIsOpen={true} />
-    </ThemeProvider>
-  </>
+      <ToDoList />
+    </>
+  );
 }
 
 export default App;
